78c7476ed60d7538cddc9f38994d733e91e71198
Matt <macavall@microsoft.com>
2024-08-09 13:43:50 -0500

    first

--- Program.cs
+++ Program.cs
@@ -15,32 +15,10 @@ internal class Program
         {
             services.AddApplicationInsightsTelemetryWorkerService();
             services.ConfigureFunctionsApplicationInsights();
-
-            // Register a Lazy<IMyService> that will be initialized later
-            services.AddSingleton(provider =>
-            {
-                return new Lazy<IMyService>(() =>
-                {
-                    // This will be executed after 30 seconds delay
-                    Console.WriteLine("IMyService initialized after delay.");
-                    return new MyService();
-                });
-            });
+            services.AddSingleton<IMyService, MyService>();
         });
 
         var ihost = host.Build();
-
-        _ = Task.Run(async () => {
-            await ihost.RunAsync();
-
-            var timer = new Timer((state) =>
-            {
-                var lazyService = ihost.Services.GetService<Lazy<IMyService>>();
-                var myService = lazyService?.Value; // This triggers the initialization
-                Console.WriteLine("IMyService is now available.");
-            }, null, TimeSpan.FromSeconds(5), Timeout.InfiniteTimeSpan);
-
-        });
 
         await ihost.RunAsync();
     }
