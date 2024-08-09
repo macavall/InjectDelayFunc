f569f7f3b394076541fb9db6c6685af7a382175d
Matt <macavall@microsoft.com>
2024-08-09 12:38:21 -0500

    first

--- Program.cs
+++ Program.cs
@@ -1,18 +1,24 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         var host = new HostBuilder()
         .ConfigureFunctionsWebApplication()
-        .ConfigureServices(services =>
+        .ConfigureServices(async services =>
         {
             services.AddApplicationInsightsTelemetryWorkerService();
             services.ConfigureFunctionsApplicationInsights();
-            services.AddSingleton<IMyService, MyService>();
+
+            _ = Task.Run(async () =>
+            {
+                await Task.Delay(5 * 1000);
+                services.AddSingleton<IMyService, MyService>();
+            });
         });
 
         var ihost = host.Build();
